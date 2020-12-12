8752530eaa78011006dba3085ddd726d6ca8998e
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-12-13 01:05:05 +0200

    Add app file

---
+++ packages/server/app.js
@@ -0,0 +1,13 @@
+const cors = require('cors');
+const express = require('express');
+const router = require('./router');
+
+
+const app = express();
+
+app.use(cors);
+app.use(express.json);
+app.use('/api', router);
+
+
+module.exports = app;
