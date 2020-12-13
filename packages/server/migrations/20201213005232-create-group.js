0eefd07db362c2c881b4519e51d9f4f45938ba06
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-12-13 02:55:38 +0200

    Add Groups migration

---
+++ packages/server/migrations/20201213005232-create-group.js
@@ -0,0 +1,49 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Groups', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      departmentId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Departments',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        allowNull: false,
+      },
+      groupCode: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      fullName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      courseNumber: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      countOfStudents: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Groups');
+  },
+};
