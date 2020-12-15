2880ce69703f7d8b457946df36e0008f4e5c5ccc
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-12-15 12:49:27 +0200

    Fix getAuditoryTypes in scheduleController

--- packages/client/schedule/src/App.js
+++ packages/client/schedule/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import {schedule} from './api/index';
 
 function App() {
+
+  const [auditoryTypes, setAuditoryTypes] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const {data: {data}} = await schedule.getAuditoryTypes();
+      setAuditoryTypes([...data]);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ul>
+        {auditoryTypes.map(
+            (item, index) => <li key={index}>{item.typeName}</li>)}
+      </ul>
   );
 }
 
--- packages/server/controllers/scheduleController.js
+++ packages/server/controllers/scheduleController.js
@@ -1,10 +1,11 @@
 const {AuditoryType} = require('../models');
 
-module.exports.getAuditoryTypes= async (req, res, next) => {
+module.exports.getAuditoryTypes = async (req, res, next) => {
 
   try {
 
-    const data = await AuditoryType.findAll({raw: true});
+    const data = await AuditoryType.findAll(
+        {attributes: ['typeName'], raw: true});
 
     res.status(200).send({data});
 
