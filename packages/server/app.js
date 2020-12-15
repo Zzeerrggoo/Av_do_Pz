9ae280d0dc4a62b7a4646c082bd589e26dcd85f3
StasTerletskiy <stanislavterletskiy1@gmail.com>
2020-12-15 12:49:05 +0200

    Fix app

--- packages/server/app.js
+++ packages/server/app.js
@@ -2,12 +2,10 @@ const cors = require('cors');
 const express = require('express');
 const router = require('./router');
 
-
 const app = express();
 
-app.use(cors);
-app.use(express.json);
+app.use(cors());
+app.use(express.json());
 app.use('/api', router);
-
 
 module.exports = app;
